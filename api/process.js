650d60c552f4e46a977d936827c6648d745d160e
LAPTOP-M0FKNJEP\LENOVO <amirhusein008@gmail.com>
2021-07-06 22:08:50 +0700

    Add last_updated

--- api/process.js
+++ api/process.js
@@ -1,5 +1,6 @@
 const page = require('./getPage')
 
+const dateUpdatePoint = 'span.o-kurs-refresh-desc span.refresh-date';
 const currencyPoint = 'td.sticky-col p';
 const erateSellPoint = 'td p[rate-type=ERate-sell]';
 const erateBuyPoint = 'td p[rate-type=ERate-buy]';
@@ -11,6 +12,11 @@ const bankNotesBuyPoint = 'td p[rate-type=BN-buy]';
 var dataList = [];
 
 module.exports = page().then(($)=>{
+    $(dateUpdatePoint).each((i,elm)=>{
+        dataList.push({
+            last_updated: $(elm).text().trim()
+        });
+    });
     $('tbody tr').each((i,elm)=>{
         dataList.push({
             mata_uang: $(elm).find(currencyPoint).text().trim(),
